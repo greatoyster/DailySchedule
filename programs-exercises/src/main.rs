2c89c2425817e20d38163749716ba5bf26a8ec5f
greatoyster <yangjq@shanghaitech.edu.cn>
2020-07-07 15:52:28 +0800

    finish rustlings

--- programs-exercises/src/main.rs
+++ programs-exercises/src/main.rs
@@ -13,7 +13,29 @@ fn create_file_structure() -> Result<(), std::io::Error> {
     Ok(())
 }
 
-fn main() -> Result<(), std::io::Error> {
-    // create_file_structure()?;
-    Ok(())
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+struct JobStatus {
+    jobs_completed: u32,
+}
+
+fn main() {
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
+    let shared_status = Arc::clone(&status);
+    thread::spawn(move || {
+        for _ in 0..10 {
+            thread::sleep(Duration::from_millis(250));
+            let mut m = shared_status.lock().unwrap();
+            (*m).jobs_completed += 1;
+            println!("{}", m.jobs_completed);
+        }
+    });
+    {
+        
+        while (*status).lock().unwrap().jobs_completed < 10 {
+            thread::sleep(Duration::from_millis(500));
+        }
+    }
 }
--- programs-exercises/target/debug/programs-exercises.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/lenovo/Documents/GitHub/DailySchedule/programs-exercises/target/debug/programs-exercises: /mnt/c/Users/lenovo/Documents/GitHub/DailySchedule/programs-exercises/src/main.rs
