c7a235bd385f673644688a4639de590d7f63cd19
greatoyster <yangjq@shanghaitech.edu.cn>
2020-07-07 11:33:04 +0800

    update notes

---
+++ programs-exercises/target/debug/programs-exercises.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/lenovo/Documents/GitHub/DailySchedule/programs-exercises/target/debug/programs-exercises: /mnt/c/Users/lenovo/Documents/GitHub/DailySchedule/programs-exercises/src/main.rs
